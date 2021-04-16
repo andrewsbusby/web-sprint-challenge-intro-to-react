f29ec32589ea43aa291dad89134a3cfb80a7909b
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-16 12:31:06 -0600

    trying to get components to render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import React, { useState, useEffect} from 'react';
+
+import React  from 'react';
 import './App.css';
 import Character from './components/Character';
 
@@ -10,22 +10,17 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-const [swCharacter, setSwCharacter] = useState([]);
 
-useEffect(() => {
-  axios.get("https://swapi.dev/api/people/")
-  .then((res) => {
-    console.log(res.data);
-    setSwCharacter(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-}, [])
+
+
+
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <Character />
+      <Character  />
+    
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,32 @@
 // Write your Character component here
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
+import axios from 'axsio';
 
 
+const [swCharacter, setSwCharacter] = useState([]);
+
+function Character(props) {
+    useEffect(() => {
+        axios.get("https://swapi.dev/api/people/")
+        .then((res) => {
+          console.log(res.data);
+          setSwCharacter(res.data.name);
+          return (
+        <div>
+            <h3>{res.data.name}</h3>
+            <p>{res.data.name}</p>
+        </div>
+        
+          )
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      }, [])
+      
 
 
-function Character() {
   
 }
 
