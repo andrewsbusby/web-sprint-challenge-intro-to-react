00d494d3eaf7ed4c308cef28cafca1396e13746d
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-14 11:31:16 -0600

    updates to app and character

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Char from './components/Character';
 
 const App = () => {
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then((res) =>{
+      setSwChar(res.data);
+      console.log(res.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }) 
+  
+  const [swChar, setSwChar] = useState();
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+import React from 'react';
+
+function Char(props){
+    const {data} = props
+
+    console.log(props);
+}
+
+export default Char;
